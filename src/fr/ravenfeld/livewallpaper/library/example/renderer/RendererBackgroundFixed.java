bb5f3d8df8780693be67dab4b8679b66ccac3acf
Alexis Lecanu <alexis.lecanu@gmail.com>
2013-09-07 14:09:56 +0200

    Remove TODO GENERATED

--- src/fr/ravenfeld/livewallpaper/library/example/renderer/RendererBackgroundFixed.java
+++ src/fr/ravenfeld/livewallpaper/library/example/renderer/RendererBackgroundFixed.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2013 Alexis Lecanu
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package fr.ravenfeld.livewallpaper.library.example.renderer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -42,7 +54,6 @@ public class RendererBackgroundFixed extends RajawaliRenderer implements
 			mBackgroundFixed = new BackgroundFixed("bg1", R.drawable.bob_bg);
 
 		} catch (TextureException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		addChild(mBackgroundFixed.getObject3D());
--- src/fr/ravenfeld/livewallpaper/library/example/renderer/RendererBackgroundGIFFixed.java
+++ src/fr/ravenfeld/livewallpaper/library/example/renderer/RendererBackgroundGIFFixed.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2013 Alexis Lecanu
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package fr.ravenfeld.livewallpaper.library.example.renderer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -42,7 +54,6 @@ public class RendererBackgroundGIFFixed extends RajawaliRenderer implements
 			mBackgroundGIFFixed = new BackgroundGIFFixed("bg1", R.drawable.bob);
 			mBackgroundGIFFixed.rewind();
 		} catch (TextureException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		addChild(mBackgroundGIFFixed.getObject3D());
@@ -56,7 +67,6 @@ public class RendererBackgroundGIFFixed extends RajawaliRenderer implements
 			try {
 				mBackgroundGIFFixed.update();
 			} catch (TextureException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
